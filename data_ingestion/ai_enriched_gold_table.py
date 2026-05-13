da580e6e6411b2eb96494011c4b79d4d747ee2e7
Melody Egwuchukwu <melodyegwuchukwu1@gmail.com>
2026-05-13 10:59:04 +0200

    AI Fleet Monitoring Assistant

--- data_ingestion/ai_enriched_gold_table.py
+++ data_ingestion/ai_enriched_gold_table.py
@@ -1,21 +1,33 @@
 %sql
-CREATE OR REPLACE TABLE lakehouse_eat_catalog.gold.fleet_sentiment AS
+CREATE OR REPLACE TABLE lakehouse_eat_catalog.gold.ai_fleet_monitoring AS
+
 SELECT
     vehicle_id,
     health_score,
-    engine_temp_avg,
+    avg_engine_temp,
     avg_speed,
+    avg_battery_health,
+    risk_level,
 
     AI_QUERY(
       'databricks-meta-llama-3-3-70b-instruct',
-      CONCAT(
-        'Analyze this fleet vehicle health. ',
-        'Health Score: ', health_score,
-        ', Engine Temp: ', engine_temp_avg,
-        ', Avg Speed: ', avg_speed,
-        '. Return sentiment (Good/Warning/Critical) ',
-        'and short operational recommendation.'
-      )
-    ) AS ai_fleet_sentiment
 
-FROM  lakehouse_eat_catalog.gold.fleet_kpis;
+      CONCAT(
+        'You are an automotive fleet monitoring assistant. ',
+        'Analyze the following vehicle telemetry KPIs and provide: ',
+        '1. Fleet health assessment ',
+        '2. Operational risk level ',
+        '3. Predictive maintenance recommendation ',
+        '4. Short explanation. ',
+
+        'Vehicle ID: ', vehicle_id,
+        ', Health Score: ', ROUND(health_score,2),
+        ', Avg Engine Temp: ', ROUND(avg_engine_temp,2),
+        ', Avg Speed: ', ROUND(avg_speed,2),
+        ', Battery Health: ', ROUND(avg_battery_health,2),
+        ', Risk Level: ', risk_level
+      )
+
+    ) AS ai_operational_insight
+
+FROM lakehouse_eat_catalog.gold.fleet_kpis;
