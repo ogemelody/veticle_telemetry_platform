d2ba94e009692b6dc1689bbf05cfd483339381fe
Melody Egwuchukwu <melodyegwuchukwu1@gmail.com>
2026-05-13 10:54:10 +0200

    AI Fleet Monitoring Assistant

--- data_ingestion/gold_fleet_kpis.py
+++ data_ingestion/gold_fleet_kpis.py
@@ -1,13 +1,26 @@
-# databricks/gold/fleet_kpis.py
-from pyspark.sql.functions import avg, col,min,max
+from pyspark.sql.functions import col, when
 
-df = spark.table("lakehouse_eat_catalog.silver.vehicle_health_features")
-
-gold = df.withColumn(
-    "health_score",
-    (col("avg_battery_health") * 0.4 +
-     (120 - col("engine_temp_avg")) * 0.3 +
-     (140 - col("avg_speed")) * 0.3)
+df = spark.table(
+    "lakehouse_eat_catalog.silver.vehicle_health_features"
 )
 
-gold.write.mode("overwrite").saveAsTable("lakehouse_eat_catalog.gold.fleet_kpis")
+gold = (
+    df.withColumn(
+        "health_score",
+        (
+            col("avg_battery_health") * 0.4 +
+            (120 - col("avg_engine_temp")) * 0.3 +
+            (140 - col("avg_speed")) * 0.3
+        )
+    )
+    .withColumn(
+        "risk_level",
+        when(col("health_score") > 85, "GOOD")
+        .when(col("health_score") > 70, "WARNING")
+        .otherwise("CRITICAL")
+    )
+)
+
+gold.write.mode("overwrite").saveAsTable(
+    "lakehouse_eat_catalog.gold.fleet_kpis"
+)
